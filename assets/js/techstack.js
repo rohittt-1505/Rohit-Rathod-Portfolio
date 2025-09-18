13efc24dbc92c17afec9bd07b873f4842d60ee1a
Rohit Rathod <132259235+rohittt-1505@users.noreply.github.com>
2025-09-18 17:35:56 +0530

    skills

--- assets/js/techstack.js
+++ assets/js/techstack.js
@@ -36,20 +36,25 @@ const techStack = [
     langDesc: "<li>JavaScript, often abbreviated JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS</li>",
   },
   {
+    langImage: "assets/images/techstack-page/react.png",
+    langName: "React",
+    langDesc: "<li>React is a free and open-source front-end JavaScript library for building user interfaces based on UI components</li>",
+  },
+  {
     langImage: "assets/images/techstack-page/node.png",
     langName: "Node",
     langDesc: "<li>Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on the V8 engine and executes JavaScript code outside a web browser</li>",
   },
-  {
-    langImage: "assets/images/techstack-page/python.png",
-    langName: "Python",
-    langDesc: "<li>Python is an interpreted high-level general-purpose programming language</li>",
-  },
-  {
-    langImage: "assets/images/techstack-page/cpp.png",
-    langName: "C++",
-    langDesc: "<li>C++ is a general-purpose programming language created by Bjarne Stroustrup as an extension of the C programming language, or C with Classes</li>",
-  },
+  // {
+  //   langImage: "assets/images/techstack-page/python.png",
+  //   langName: "Python",
+  //   langDesc: "<li>Python is an interpreted high-level general-purpose programming language</li>",
+  // },
+  // {
+  //   langImage: "assets/images/techstack-page/cpp.png",
+  //   langName: "C++",
+  //   langDesc: "<li>C++ is a general-purpose programming language created by Bjarne Stroustrup as an extension of the C programming language, or C with Classes</li>",
+  // },
   {
     langImage: "assets/images/techstack-page/git.png",
     langName: "Git",
@@ -60,16 +65,11 @@ const techStack = [
     langName: "Bootstrap",
     langDesc: "<li>Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development</li>",
   },
-  {
-    langImage: "assets/images/techstack-page/react.png",
-    langName: "React",
-    langDesc: "<li>React is a free and open-source front-end JavaScript library for building user interfaces based on UI components</li>",
-  },
-  {
-    langImage: "assets/images/techstack-page/c.png",
-    langName: "C",
-    langDesc: "<li>C is a general-purpose, procedural computer programming language supporting structured programming, lexical variable scope, and recursion, with a static type system</li>"
-  },
+  // {
+  //   langImage: "assets/images/techstack-page/c.png",
+  //   langName: "C",
+  //   langDesc: "<li>C is a general-purpose, procedural computer programming language supporting structured programming, lexical variable scope, and recursion, with a static type system</li>"
+  // },
 ];
 
 const displayTechStacksCards = () => {
